61ab577e7b1364d98ce81e8bc8b0d4d45ecaefce
Sangeerthan <152469911+SG039@users.noreply.github.com>
2025-04-27 10:31:27 +0530

    edit

--- doctor-appointment-system/routes/web.php
+++ doctor-appointment-system/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    
+    Route::resource('appointments', AppointmentController::class);
+    Route::resource('doctors', DoctorController::class);
 });
+
+
+require __DIR__.'/auth.php';
